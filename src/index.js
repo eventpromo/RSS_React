155d2d4ec0ab3a3a38f7fd908b363f7e7a7bb2b2
Siarhei <eventpromoby@gmail.com>
2020-02-09 17:53:30 +0300

    dev server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app.jsx';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+document.body.innerHTML = '<div id="root"></div>';
+ReactDOM.render(<App />, document.getElementById('root'));
